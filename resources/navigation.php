8f69221b3d1a5e257b99c1043f6adc253a9e0d34
Carter Irlmeier <carter.irlmeier@my.simpson.edu>
2018-11-01 18:31:32 -0500

    test2

--- resources/navigation.php
+++ resources/navigation.php
@@ -1,11 +1,44 @@
   <!-- Navigation Bar -->
-    <h1>Strawberry University</h1>
-    <ul class="nav navbar-nav">
-      <li><a href="../public_html/index.php">Home</a></li>
-      <li><a href="../public_html/about.php">About</a></li>
-      <li><a href="../public_html/admissions.php">Admissions</a></li>
-      <li><a href="../public_html/student_life.php">Student Life</a></li>
-      <li><a href="../public_html/athletics.php">Athletics</a></li>
-      <li><a href="../public_html/sign_up.php">Sign Up</a></li>
-      <li><a href="../public_html/deploy_script.php">Deploy Script</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <p class="navbar-btn">
+                <a href="index.php" class="btn btn-danger">Home</a>
+            </p>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="admissions.php">Admissions</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="academics.php">Academics</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="student_life.php">Student Life</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="athletics.php">Athletics</a>
+          </li>
+          
+          <li>
+            <p class="navbar-btn">
+                <a href="sign_up.php" class="btn btn-secondary">Sign up for a visit.</a>
+            </p>
+          </li>
+          <li>
+        </ul>
+      </div>
+    </nav>
