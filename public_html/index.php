5aa57b6feea7b84707c951531115e09b7518b5f1
Carter Irlmeier <carter.irlmeier@my.simpson.edu>
2018-11-01 18:31:10 -0500

    test

--- public_html/index.php
+++ public_html/index.php
@@ -35,50 +35,7 @@ nav div ul li{
 </head>
 
 <body>
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <p class="navbar-btn">
-                <a href="index.php" class="btn btn-danger">Home</a>
-            </p>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="admissions.php">Admissions</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="academics.php">Academics</a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="student_life.php">Student Life</a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="athletics.php">Athletics</a>
-          </li>
-          
-          <li>
-            <p class="navbar-btn">
-                <a href="sign_up.php" class="btn btn-secondary">Sign up for a visit.</a>
-            </p>
-          </li>
-          <li>
-        </ul>
-      </div>
-    </nav>
-
+  <?php include '../resources/navigation.php'; ?>
 <div class="jumbotron">
     <h1 class="shadow"> 
     <center>
